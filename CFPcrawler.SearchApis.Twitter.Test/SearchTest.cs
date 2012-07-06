fa6cbd8472e9a7f97bb88f2f68350dcb1d5c96fd
Priscila Mayumi sato <mayumisatox@gmail.com>
2012-07-06 17:01:03 -0300

    Organize the projects and create the first test

---
+++ CFPcrawler.SearchApis.Twitter.Test/SearchTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using CFPcrawler.SearchApis.Twitter;
+using CFPcrawler.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CFPcrawler.SearchApis.Twitter.Test
+{
+    [TestClass]
+    public class SearchTest
+    {
+        public SearchTest()
+        {
+            
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void Search_Anything_Return_Anything()
+        {
+            Search search = new Search();
+            string result = search.Search();
+            Assert.AreNotEqual(result, ResultTypes.ComplexQuery);
+        }
+    }
+}
---
+++ Keys/DbHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CFPcrawler.Keys
+{
+    public class DbHelper
+    {
+        public static string ConectionString
+        {
+            get
+            {
+#if DEBUG
+                return "put here";
+#else
+                return "put here";
+#endif
+            }
+        }
+    }
+}
